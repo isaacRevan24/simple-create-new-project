5132cb8c14298a3bf1880844cc974cef6a2a5ceb
isaacRevan24 <isaac2498atencio@criptext.com>
2020-06-04 19:07:55 -0500

    Added comments in SendProjectSolicitude with the manager info

--- services/solicitudeReques.go
+++ services/solicitudeReques.go
@@ -6,6 +6,10 @@ import "fmt"
 // friends added by the manager / creator of the project
 func SendProjectSolicitude(membersRequest map[string]string, manager *map[string][2]string, project string, ID string) {
 
+	/*
+		manager[0] User Name
+		manager[1] User ID
+	*/
 	for _, info := range *manager {
 		fmt.Println("Solicitude sended by " + info[0] + " of the username" + info[1] + "at the project:" + project)
 	}
